e3cdce37432b53f9f647f577904c585a498fc137
Scott <ScottSherod@Gmail.com>
2018-05-06 20:56:42 -0700

    added plot stuff

--- Call.m
+++ Call.m
@@ -1,56 +1,41 @@
 clc
 clear all
 close all
-M0 = 2
-alt = 20000
-pi_c = 20
-Tt4 = 2000
-Tt7 = 2220
-d = linspace(0.1,2,1000)
-Tt = zeros(length(pi_c),7)
-Pt = zeros(length(pi_c),7)
+M0 = 0;
+alt = 0;
+pi_c = 20;
+Tt4 = 2000;
+Tt7 = 2220;
+d = linspace(0,1.5,100);
+Tt = zeros(length(pi_c),7);
+Pt = zeros(length(pi_c),7);
+size = 20;
 
 for i = 1:length(d)
-[Tt(i,:), Pt(i,:), M9(i), f(i), S(i), F_m0(i), T(i), D_add(i)] = realTJAB(M0, alt, pi_c, Tt4, Tt7, d(i))
+[Tt0(i,:), Pt0(i,:), M90(i), f0(i), S0(i), F_m00(i), T0(i), D_add0(i)] = realTJAB(M0, alt, pi_c, Tt4, Tt7, d(i));
 end
 
-subplot(2,3,1)
-plot(d,M9)
-title('Exit Mach vs Compressor Pressure Ratio')
-xlabel('Pic')
-ylabel('Exit Mach Number')
-
-subplot(2,3,2)
-plot(d,f)
-title('Air Fuel Ratio vs Compressor Pressure Ratio')
-xlabel('Pic')
-ylabel('Air Fuel Ratio')
-
-subplot(2,3,3)
-plot(d,S)
-title('Specific Fuel Consumption vs Compressor Pressure Ratio')
-xlabel('Pic')
-ylabel('Specific Fuel Consumption')
-
-subplot(2,3,4)
-plot(d,F_m0)
-title('Specific Thrust vs Compressor Pressure Ratio')
-xlabel('Pic')
-ylabel('Specific Thrust')
-
-subplot(2,3,5)
-
-
-plot(d,T)
-title('Thrust vs Diameter of Inlet')
-xlabel('Diameter of Inlet [m]')
-ylabel('Thrust [N]')
+q = plot(d,T0);
+title('Thrust vs Diameter of Inlet','fontsize',size);
+xlabel('Diameter of Inlet [m]','fontsize',size);
+ylabel('Thrust [N]','fontsize',size);
 set(gcf, 'color' ,'white');
-x = [0,max(d)]
-y = [160000 160000]
-z = line(x,y)
-z.Color = 'r'
-legend ('Thrust [N]','Takeoff Thrust Required [N]')
+x = [0,max(d)];
+y = [160000 160000];
+z = line(x,y);
+ylim([0,350000]);
+z.Color = 'r';
+grid on
+leg = legend ('Thrust [N]','Takeoff Thrust Required [N]','location','northwest');
+leg.FontSize = size;
+set(findall(gca, 'Type', 'Line'),'LineWidth',2);
+xt = get(gca, 'XTick');
+set(gca, 'FontSize', 16)
+%%
+d0 = 0.75
 
-
+pic = linspace(2,40,100)
+for i = 1:length(pic)
+[Tt1(i,:), Pt1(i,:), M91(i), f1(i), S1(i), F_m01(i), T1(i), D_add1(i)] = realTJAB(M0, alt, pic(i), Tt4, Tt7, d0)
+end
 
